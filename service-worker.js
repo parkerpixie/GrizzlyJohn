b5a34f612a3c71a59e60c0f8a36d6d8dbf31f170
parkerpixie <parkerpixiedust@gmail.com>
2026-08-18 20:47:05 -0500

    Refresh PWA cache for Home overhaul

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'grizzlyjohn-v1';
+const CACHE_NAME = 'grizzlyjohn-v2-home-wisdom';
 const APP_SHELL = [
   './',
   './index.html',
@@ -7,6 +7,7 @@ const APP_SHELL = [
   './oracle-cards.css',
   './install.css',
   './dbt-cards.css',
+  './enhancements.css',
   './app.js',
   './data.js',
   './weather.js',
