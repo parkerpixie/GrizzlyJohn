2f15e498d5cee6a6222d8af143fa11e3dbcbf943
parkerpixie <parkerpixiedust@gmail.com>
2026-08-21 16:04:39 -0500

    Roam V2 cleanup
    * Build Roam V2 adventure hub
    
    * Load Roam V2 organizer
    
    * Cache Roam V2 assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'grizzlyjohn-v13-brene-reflection';
+const CACHE_NAME = 'grizzlyjohn-v14-roam-v2';
 const APP_SHELL = [
   './',
   './index.html',
@@ -27,6 +27,7 @@ const APP_SHELL = [
   './listen-upgrades.js',
   './jen-quests.js',
   './brene-reflection.js',
+  './roam-v2.js',
   './backpack-suggestion.html',
   './manifest.json',
   './GrizzlyJohn%20App%20Icon.png',
